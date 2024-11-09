e8a183e87a3361153c49f6d87597988b07ce085f
runhao <1549971272@qq.com>
2024-11-09 13:57:56 +0800

    feat: 暂存

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="index"> 首页 </div>
+    <a @click="handleJump"> 点击跳转登录 </a>
 </template>
 
 <script>
+import {useRouter} from "vue-router";
+
 export default {
     name: "Index",
     components:{},
     setup() {
-        
+      const router = useRouter()
+      const handleJump = () => {
+        router.push('/login')
+		  }
+      return{handleJump}
     },
 }
 </script>
