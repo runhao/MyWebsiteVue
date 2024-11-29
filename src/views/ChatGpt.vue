15fad06d47e98d5cc82c195285eabd7898c60915
runhao <1549971272@qq.com>
2024-11-29 17:01:18 +0800

    feat: 修改url

--- src/views/ChatGpt.vue
+++ src/views/ChatGpt.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <!-- 嵌套外部网页 -->
     <iframe
-      src="https://naodongzhizao.com:3000"
+      src="https://localhost:3000"
       width="100%"
       height="800"
       allowfullscreen
