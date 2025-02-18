76a6c223b007091ee36cb174648f0ed7f5ea9cce
runhao <1549971272@qq.com>
2025-02-18 14:02:35 +0800

    feat: 修改项目备案描述

--- src/components/Distribution.vue
+++ src/components/Distribution.vue
@@ -9,6 +9,7 @@
     <a href="https://beian.miit.gov.cn">
       蜀ICP备2024101395号-2
     </a>
+    <p>本站只用于测试和体验目的，请自觉遵守您当地法律法规，切勿用于非法用途，本站不承担任何法律责任。</p>
     <p>Contact: 1549971272@qq.com runhao</p>
   </div>
 </template>
